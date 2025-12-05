a2bc9852d95991c6111742bdabec32c8b64618fd
xinom4966 <sd.dessay@gmail.com>
2025-12-05 14:11:14 +0100

    need to merge feat_sfx

--- Assets/Scripts/MeteoEvents/Tornado.cs
+++ Assets/Scripts/MeteoEvents/Tornado.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class Tornado : MeteoEvent
 {
-    
+    [SerializeField] private float maxStepDist;
+    [SerializeField] private float speed;
+    private Vector3 destination;
+
+    private void OnEnable()
+    {
+        TickSystem.ticked += OnTicked;
+    }
+
+    private void OnTicked()
+    {
+        if (transform.position == destination)
+        {
+            destination = Random.insideUnitSphere * maxStepDist;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+    }
+
+    private void OnDisable()
+    {
+        TickSystem.ticked -= OnTicked;
+    }
 }
