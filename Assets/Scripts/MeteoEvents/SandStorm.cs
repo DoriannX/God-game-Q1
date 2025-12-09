fba9364c9fab09eb544ca5a9184f32494d4bc74f
xinom4966 <sd.dessay@gmail.com>
2025-12-09 11:56:09 +0100

    Started the random events

--- Assets/Scripts/MeteoEvents/SandStorm.cs
+++ Assets/Scripts/MeteoEvents/SandStorm.cs
@@ -2,5 +2,60 @@ using UnityEngine;
 
 public class SandStorm : MeteoEvent
 {
-    
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject sandPrefab;
+    [SerializeField] private float sandTickInterval;
+    private Vector3 destination;
+    private float timer = 0;
+
+    private void OnEnable()
+    {
+        PlaceAtCameraCenter();
+    }
+
+    private void Update()
+    {
+        if (timer >= sandTickInterval)
+        {
+            PlaceAtCameraCenter();
+
+            destination = Random.insideUnitCircle * mainCamera.fieldOfView;
+            float temp = destination.y;
+            destination.y = 0;
+            destination.z = temp;
+            timer = 0;
+        }
+        Vector3Int hexDest = TilemapManager.instance.WorldToHexAxial(destination);
+        TilemapManager.instance.SpawnTileAt(hexDest, sandPrefab);
+        timer += Time.deltaTime;
+    }
+
+    private void PlaceAtCameraCenter()
+    {
+        Vector2 screenCenter;
+        screenCenter.x = Screen.width/2;
+        screenCenter.y = Screen.height/2;
+        Ray ray = mainCamera.ScreenPointToRay(screenCenter);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float distance;
+
+            if (groundPlane.Raycast(ray, out distance))
+            {
+                transform.position = ray.GetPoint(distance);
+            }
+            else
+            {
+                Debug.LogWarning("Could not calculate world position");
+                return;
+            }
+        }
+    }
 }
--- Assets/Scripts/MeteoEvents/Tornado.cs
+++ Assets/Scripts/MeteoEvents/Tornado.cs
@@ -4,24 +4,45 @@ public class Tornado : MeteoEvent
 {
     [SerializeField] private float maxStepDist;
     [SerializeField] private float speed;
+    [SerializeField] private Camera mainCamera;
     private Vector3 destination;
 
     private void OnEnable()
     {
-        TickSystem.ticked += OnTicked;
+        //TickSystem.ticked += OnTicked;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = hit.point;
+        }
     }
 
-    private void OnTicked()
+    private void Update()
     {
         if (transform.position == destination)
         {
-            destination = Random.insideUnitSphere * maxStepDist;
+            destination = Random.insideUnitCircle * maxStepDist;
+            float temp = destination.y;
+            destination.y = 0;
+            destination.z = temp;
         }
         transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
     }
 
-    private void OnDisable()
+    private void OnCollisionEnter(Collision collision)
+    {
+        House houseHit = collision.gameObject.GetComponent<House>();
+        if (houseHit != null)
+        {
+            GhostManager.instance.UnregisterGhostInHouse(houseHit);
+            Destroy(houseHit.gameObject);
+        }
+    }
+
+    /*private void OnDisable()
     {
         TickSystem.ticked -= OnTicked;
-    }
+    }*/
 }
