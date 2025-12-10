00f58ce6dd746d74eefbd5f5e2d852dc5f024a0a
Lawice <nohlan.chillmoonlight@gmail.com>
2025-12-10 10:01:58 +0100

    Set up BT

--- Assets/spawnghost.cs
+++ Assets/spawnghost.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class spawnghost : MonoBehaviour
 {
+    
+    //TODO: REMOVE
     public GameObject ghost;
     public void SpawnGhost() {
         Instantiate(ghost, transform.position, transform.rotation);
