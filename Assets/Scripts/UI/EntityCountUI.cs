d39e905c748ddcc8287c41aaeaab3dcf069bfaeb
Lawice <nohlan.chillmoonlight@gmail.com>
2025-12-15 17:03:07 +0100

    Compteur Fix

---
+++ Assets/Scripts/UI/EntityCountUI.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class EntityCountUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text entityCountText;
+    [SerializeField] private EntityType entity;
+
+    private void Start() {
+        UpdateUI(entity, 0);
+        EntityManager.instance.onEntityChanged += UpdateUI;
+    }
+
+    private void UpdateUI(EntityType entityType, int count) {
+        if (entityType == entity) {
+            entityCountText.text = count.ToString();
+        }
+    }
+}
