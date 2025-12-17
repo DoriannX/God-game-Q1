218c014943b1de9be760a15695b18ef24e946abb
Lawice <nohlan.chillmoonlight@gmail.com>
2025-12-17 14:56:26 +0100

    grow for entity

--- Assets/Scripts/Components/Grow/EntityGrowComponent.cs
+++ Assets/Scripts/Components/Grow/EntityGrowComponent.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityGrowComponent : GrowComponent {
     [SerializeField] private List<AnimationClip> growthStages;
     private Animator animator;
-    
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        if (growthStages.Count > 0) {
+            animator.Play(growthStages[0].name);
+        }
+    }
+
     public override void Grow() {
-        
+        growthProgressPercent += growthRate;
+        int stageIndex = Mathf.Min((int)(growthProgressPercent * growthStages.Count), growthStages.Count - 1);
+        animator.Play(growthStages[stageIndex].name);
+
+        if (growthProgressPercent >= 1f) {
+            DoneGrowing();
+        }
     }
 }
