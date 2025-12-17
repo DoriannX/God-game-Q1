0e7e9d52a15a7d602b5970140503ae1c5b9c1086
Lawice <nohlan.chillmoonlight@gmail.com>
2025-12-17 13:44:46 +0100

    fix serialize fix

--- Assets/Scripts/Entities/PosableEntity.cs
+++ Assets/Scripts/Entities/PosableEntity.cs
@@ -6,7 +6,7 @@ public class PosableEntity : Posable
 
     private void Awake()
     {
-        entityType = GetComponent<EntityAI>().entityType;
+        entityType = GetComponent<EntityAI>().GetEntityType();
     }
 }
 
